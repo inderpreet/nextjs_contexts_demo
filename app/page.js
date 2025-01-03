32aa516449b5d2aa74d7afea6f81f27c26d1935a
Inderpreet Singh <ip_v1@hotmail.com>
2025-01-02 23:04:55 -0500

    fix imports

--- app/page.js
+++ app/page.js
@@ -1,6 +1,5 @@
 import Login from "./components/Login";
 import Profile from "./components/Profile";
-import { AppWrapper } from "./contexts/AppContext";
 import UserContextProvider from "./contexts/UserContextProvider";
 
 function Home({ Component, pageProps }) {
